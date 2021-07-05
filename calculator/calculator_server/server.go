77e1e3eb7c4ce1274ed1b61fdb0d2a1b06c1fd48
Aditya Hajare <aditya43@gmail.com>
2021-07-05 13:46:16 +0530

    ComputeAverage server

--- calculator/calculator_server/server.go
+++ calculator/calculator_server/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"net"
 
@@ -44,6 +45,30 @@ func (*server) PrimeNumberDecomposition(req *calculatorpb.PrimeNumberDecompositi
 	}
 
 	return nil
+}
+
+func (*server) ComputeAverage(stream calculatorpb.CalculatorService_ComputeAverageServer) error {
+	fmt.Println("Received ComputeAverage RPC")
+
+	sum := int32(0)
+	count := 0
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			average := float64(sum) / float64(count)
+			stream.SendAndClose(&calculatorpb.ComputeAverageResponse{
+				Average: average,
+			})
+		}
+
+		if err != nil {
+			log.Fatalf("Error reading client stream: %v", err)
+		}
+
+		sum += req.GetNumber()
+		count++
+	}
 }
 
 func main() {
